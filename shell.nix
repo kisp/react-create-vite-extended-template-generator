e4199da42eec376eb01101e40f12c9d02d94062b
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-04 22:37:32 +0200

    Add tree-sitter grammars for Emacs

--- shell.nix
+++ shell.nix
@@ -1,11 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  # How to install and use tree-sitter grammars? · Issue #341 · nix-community/emacs-overlay
+  # https://github.com/nix-community/emacs-overlay/issues/341
+  myEmacs = (pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (epkgs: with epkgs; [
+    vterm
+    treesit-grammars.with-all-grammars
+  ]);
+
+in
+
 pkgs.mkShell {
   name = "react-create-vite-extended-template-shell";
   buildInputs = with pkgs; [
     (import ./default.nix { inherit pkgs; }).buildInputs
     pkgs.yamllint
-    pkgs.emacs-nox
+    myEmacs
     pkgs.emacsPackages.yaml-mode
     pkgs.emacsPackages.whitespace-cleanup-mode
     pkgs.emacsPackages.flycheck
